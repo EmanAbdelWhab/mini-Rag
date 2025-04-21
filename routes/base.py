f31fc923053f0c27c73efa21f355a397172207cd
EmanAbdelwhab <ea6677342@gmail.com>
2025-04-22 01:36:00 +0200

    App Update

---
+++ routes/base.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+import os
+
+# Define API router with versioned prefix and tag
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["API v1"],
+)
+
+# Root route of the API
+@base_router.get("/")
+async def welcome():
+    app_name =os.getenv("APP_NAME")
+    app_version = os.getenv("App_VERSION")
+    return {
+        "app_name": app_name,
+        "app_version": app_version,
+        }
