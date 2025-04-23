9dace0026d4cf3ee65bd247e1235a56c7687e151
EmanAbdelwhab <ea6677342@gmail.com>
2025-04-23 16:59:32 +0200

    feat: add data_router to FastAPI app for handling data-related routes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import base # Import the base router from routes/base.py
+from routes import data # Import the data router from routes/data.py
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -10,3 +11,4 @@ app = FastAPI(
 
 # Register the router
 app.include_router(base.base_router)
+app.include_router(data.data_router)
